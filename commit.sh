63016f10647d680a8962c89882f562db82b54369
Yan Brasiliano Silva Penalva <yanpenabr@gmail.com>
2023-04-20 10:41:54 -0300

    Update commit.sh

--- commit.sh
+++ commit.sh
@@ -9,26 +9,41 @@ read -p "Enter commit message: " commit_msg
 # Commit changes
 git commit -m "$commit_msg"
 
-# Push changes to remote
-git push
+# Get current branch
+current_branch=$(git rev-parse --abbrev-ref HEAD)
 
-# Get last tag
-last_tag=$(git describe --abbrev=0 --tags)
+# Push current branch to remote
+git push origin $current_branch
 
-# Show last tag
-echo
-echo "The last tag created was $last_tag"
+# Prompt to create tag
+read -p "Do you want to create a tag? (y/n) " create_tag
 
-# Prompt for tag name
-read -p "Enter tag: " tag_name
+if [[ $create_tag == "y" || $create_tag == "Y" ]]; then
+  # Check if current branch is develop, master, or release/number
+  if [[ $current_branch == "develop" || $current_branch == "master" || $current_branch =~ ^release\/[0-9]+$ ]]; then
+    # Get last tag
+    last_tag=$(git describe --abbrev=0 --tags)
 
-# Prompt for tag message
-read -p "Enter tag message: " tag_msg
+    # Show last tag
+    echo
+    echo "The last tag created was $last_tag"
 
-# Create and push tag
-git tag -a $tag_name -m "$tag_msg"
-git push origin $tag_name
+    # Prompt for tag name
+    read -p "Enter tag: " tag_name
 
-echo "Commit and tag pushed."
-echo
-echo "New tag is $tag_name"
+    # Prompt for tag message
+    read -p "Enter tag message: " tag_msg
+
+    # Create and push tag
+    git tag -a $tag_name -m "$tag_msg"
+    git push origin $tag_name
+
+    echo "Commit and tag pushed."
+    echo
+    echo "New tag is $tag_name"
+  else
+    echo "Cannot create tag on branch $current_branch. Only allowed on develop, master, or release/number. Exiting..."
+  fi
+else
+  echo "No tag was created. Exiting..."
+fi
